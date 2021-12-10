18e7b04d55c515f5e3b18e649d10a6d7c548ef31
Mark <mark.j.wee@gmail.com>
2021-12-10 13:42:53 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(currentStreet) {
+    if (currentStreet >= 42){
+        return currentStreet - 42
+    }
+    else if (currentStreet < 42){
+        return 42 - currentStreet
+    }
+}
+
+function distanceFromHqInFeet(currentStreet) {
+    if (currentStreet >= 42){
+        return ( (currentStreet - 42) * 264 )
+    }   else {
+        return ( (42 - currentStreet) * 264 )
+       }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (Math.abs(start - destination) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+    if ( (Math.abs(start - destination) * 264) < 400 ){
+        return 0
+    }
+    else if ( ( (Math.abs(start - destination) * 264) >= 400 ) && ( (Math.abs(start - destination) * 264) <= 2000 ) ){
+        return ( 0.02 * ( (Math.abs(start - destination) * 264) - 400 ) )
+   }
+   else if ( ( (Math.abs(start - destination) * 264) > 2000) && ( (Math.abs(start - destination) * 264) < 2500 ) ){
+       return 25
+   }
+   else if ( (Math.abs(start - destination) * 264) >= 2500){
+       return "cannot travel that far"
+   }
+}
